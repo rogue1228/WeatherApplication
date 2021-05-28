9908b1da4dad1395ca255368e7bf41f4c344a49b
JunhwaLee <leejunhwa88@gmail.com>
2021-05-29 03:06:42 +0900

    viewModelModule DI 추가

---
+++ app/src/main/java/com/junhwa/weatherapplication/application/di/viewModelModule.kt
@@ -0,0 +1,11 @@
+package com.junhwa.weatherapplication.application.di
+
+import com.junhwa.weatherapplication.ui.main.MainViewModel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel {
+        MainViewModel()
+    }
+}
